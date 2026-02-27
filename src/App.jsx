572c2291abe17cb1ce2a9098d47612c2b5f2e2b3
trishawakode1 <trishawakode1@gmail.com>
2026-02-27 11:51:49 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Homepage from './pages/Homepage'
+import Landing from './pages/Landing'
+import GetInitDetails from './pages/GetInitDetails'
+
+const App = () => {
+
+  return (
+    <>
+    <GetInitDetails/>
+    </>
+  )
+}
+
+export default App
